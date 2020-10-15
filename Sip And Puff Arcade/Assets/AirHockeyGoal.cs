a1053b80c0091b35a31ea9466b61de84320c6729
dylanjwendt <dylanjwendt@aol.com>
2020-10-15 18:30:22 -0500

    Air Hockey Improvements
    Now in Technicolor

---
+++ Sip And Puff Arcade/Assets/AirHockeyGoal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AirHockeyGoal : MonoBehaviour
+{
+    public GameObject puck;
+    
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = puck.GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Puck"))
+        {
+            puck.GetComponent<PuckMovement>().Reset();
+        }
+    }
+}
