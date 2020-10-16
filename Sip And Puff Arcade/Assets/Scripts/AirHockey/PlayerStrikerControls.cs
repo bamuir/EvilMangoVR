b338ad4d0514e2805474dea179425f02e4345477
dylanjwendt <dylanjwendt@aol.com>
2020-10-15 23:02:21 -0500

    Fixed Project Heirarchy
    Now all the scripts have a home.

---
+++ Sip And Puff Arcade/Assets/Scripts/AirHockey/PlayerStrikerControls.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStrikerControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y, gameObject.transform.localPosition.z + 0.01f);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y, gameObject.transform.localPosition.z - 0.01f);
+        }
+        if(gameObject.transform.localPosition.z > 0.825f)
+        {
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y, 0.825f);
+        }
+        if (gameObject.transform.localPosition.z < -0.825f)
+        {
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y, -0.825f);
+        }
+    }
+}
---
+++ Sip And Puff Arcade/Assets/Scripts/AirHockey/PuckHitStriker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PuckHitStriker : MonoBehaviour
+{
+    public float bounceForce = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.gameObject.CompareTag("Striker"))
+        {
+            Material[] mat = gameObject.GetComponent<MeshRenderer>().materials;
+            mat[1] = collision.collider.gameObject.GetComponent<MeshRenderer>().material;
+            gameObject.GetComponent<MeshRenderer>().materials = mat;
+            gameObject.GetComponent<Rigidbody>().AddForce((collision.collider.gameObject.transform.position - gameObject.transform.position).normalized * bounceForce);
+        }
+    }
+}
