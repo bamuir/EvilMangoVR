3db4f2548f677710bed6f03a218d072852b7dd3b
Sam3077 <sam.hirsch66@gmail.com>
2020-10-16 22:50:11 -0600

    added control scripts

---
+++ Sip And Puff Arcade/Assets/Scripts/Control/TranslationLayer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum ButtonCode
+{
+    KeyLeft,
+    KeyRight,
+    KeyBack,
+    KeyFoward
+}
+
+/**
+ * This class translates from available input sources to a generic type 'TranslationLayer.KeyCode'
+ * Should be actively updated as needed with new patterns for the Sip and Puff device.
+ * Largely aliases the Input public functions
+ */
+public class TranslationLayer : MonoBehaviour
+{
+    public VRControllerAdapter VRAdapter;
+    public static TranslationLayer instance = null;
+
+    public TranslationLayer()
+    {
+        TranslationLayer.instance = this;
+    }
+
+    /**
+     * Returns true in the frame where the button is pressed
+     */
+    public bool GetButtonDown(ButtonCode key)
+    {
+        KeyCode systemKey;
+        switch(key)
+        {
+            case ButtonCode.KeyLeft:
+                systemKey = KeyCode.LeftArrow;
+                break;
+            case ButtonCode.KeyRight:
+                systemKey = KeyCode.RightArrow;
+                break;
+            case ButtonCode.KeyBack:
+                systemKey = KeyCode.DownArrow;
+                break;
+            case ButtonCode.KeyFoward:
+                systemKey = KeyCode.UpArrow;
+                break;
+            default:
+                throw new ArgumentException("Unknown KeyCode");
+        }
+        return Input.GetKeyDown(systemKey) || VRAdapter.GetButtonDown(key);
+    }
+
+    /**
+     * Returns true as long as the button is held
+     */
+    public bool GetButton(ButtonCode key)
+    {
+        KeyCode systemKey;
+        switch (key)
+        {
+            case ButtonCode.KeyLeft:
+                systemKey = KeyCode.LeftArrow;
+                break;
+            case ButtonCode.KeyRight:
+                systemKey = KeyCode.RightArrow;
+                break;
+            case ButtonCode.KeyBack:
+                systemKey = KeyCode.DownArrow;
+                break;
+            case ButtonCode.KeyFoward:
+                systemKey = KeyCode.UpArrow;
+                break;
+            default:
+                throw new ArgumentException("Unknown KeyCode");
+        }
+        return Input.GetKey(systemKey) || VRAdapter.GetButton(key);
+    }
+}
---
+++ Sip And Puff Arcade/Assets/Scripts/Control/VRControllerAdapter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class VRControllerAdapter : MonoBehaviour
+{
+    public SteamVR_Input_Sources hand;
+
+    public SteamVR_Action_Boolean forwardPressed;
+    public SteamVR_Action_Boolean rightPressed;
+    public SteamVR_Action_Boolean leftPressed;
+    public SteamVR_Action_Boolean backPressed;
+
+    public bool GetButtonDown(ButtonCode keyCode)
+    {
+        switch (keyCode)
+        {
+            case ButtonCode.KeyFoward:
+                return forwardPressed.GetStateDown(hand);
+            case ButtonCode.KeyRight:
+                return rightPressed.GetStateDown(hand);
+            case ButtonCode.KeyBack:
+                return backPressed.GetStateDown(hand);
+            case ButtonCode.KeyLeft:
+                return leftPressed.GetStateDown(hand);
+            default:
+                throw new ArgumentException("Unknown keyCode");
+        }
+    }
+
+    public bool GetButton(ButtonCode keyCode)
+    {
+        switch (keyCode)
+        {
+            case ButtonCode.KeyFoward:
+                return forwardPressed.GetState(hand);
+            case ButtonCode.KeyRight:
+                return rightPressed.GetState(hand);
+            case ButtonCode.KeyBack:
+                return backPressed.GetState(hand);
+            case ButtonCode.KeyLeft:
+                return leftPressed.GetState(hand);
+            default:
+                throw new ArgumentException("Unknown keyCode");
+        }
+    }
+}
