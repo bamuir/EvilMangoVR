fc54d31b6e1c09c36027530ac223de8b98c49627
Aarceo1 <aarceo1@bruinmail.slcc.edu>
2020-10-26 01:13:04 -0600

    Crane game machine material implemented for game body and back ground toys. Added movement script for crane. Added extra light to scence. Crane game collision still not working

---
+++ Sip And Puff Arcade/Assets/Scripts/CraneGame/ClawController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //switch ()
+        //{
+        //    case 1:
+        //        Console.WriteLine("Case 1");
+        //        break;
+        //    case 2:
+        //        Console.WriteLine("Case 2");
+        //        break;
+        //    default:
+        //        Console.WriteLine("Default case");
+        //        break;
+        //}
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate (new Vector3 (-2f, 0f, 0f) * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(new Vector3(2f, 0f, 0f) * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(new Vector3(0f, -2f, 0f) * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(new Vector3(0f, 2f, 0f) * Time.deltaTime);
+        }
+    }
+}
