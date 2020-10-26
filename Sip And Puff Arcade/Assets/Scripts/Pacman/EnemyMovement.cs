a6a59a0d83157e41a9e8928c96bd6e6fde8cc231
Robert Kay <rdkjr11@hotmail.com>
2020-10-26 15:55:27 -0600

    Pacman Game In Progress

---
+++ Sip And Puff Arcade/Assets/Scripts/Pacman/EnemyMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    int currentPoint = 0;
+    public float speed = 0.3f;
+
+    Rigidbody2D rb;
+    Vector2 direction;
+    Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.position != waypoints[currentPoint].position)
+        {
+            Vector2 point = Vector2.MoveTowards(transform.position, waypoints[currentPoint].position, speed);
+
+            GetComponent<Rigidbody2D>().MovePosition(point);
+        }
+        else
+        {
+            currentPoint++;
+            currentPoint %= waypoints.Length;
+        }
+
+        Vector2 direction = waypoints[currentPoint].position - transform.position;
+        GetComponent<Animator>().SetFloat("DirX", direction.x);
+        GetComponent<Animator>().SetFloat("DirY", direction.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+}
---
+++ Sip And Puff Arcade/Assets/Scripts/Pacman/Movement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 0.4f;
+    Vector2 dest = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dest = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+
+        if ((Vector2)transform.position == dest)
+        {
+            if (Input.GetKey(KeyCode.UpArrow) && Valid(Vector2.up))
+            {
+                dest = (Vector2)transform.position + Vector2.up;
+            }
+            if (Input.GetKey(KeyCode.RightArrow) && Valid(Vector2.right))
+            {
+                dest = (Vector2)transform.position + Vector2.right;
+            }
+            if (Input.GetKey(KeyCode.DownArrow) && Valid(-Vector2.up))
+            {
+                dest = (Vector2)transform.position - Vector2.up;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow) && Valid(-Vector2.right))
+            {
+                dest = (Vector2)transform.position - Vector2.right;
+            }
+        }
+
+        Vector2 dir = dest - (Vector2)transform.position;
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
+    }
+
+    bool Valid(Vector2 dir)
+    {
+        Vector2 position = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(position + dir, position);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
+}
