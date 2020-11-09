884d45b4ae899efee03fa598558cd5ca44c8bd5d
Sam3077 <sam.hirsch66@gmail.com>
2020-11-09 15:06:55 -0700

    merged master

--- Sip And Puff Arcade/Assets/Scripts/AirHockey/GameSwitch.cs
+++ Sip And Puff Arcade/Assets/Scripts/AirHockey/GameSwitch.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameSwitch : MonoBehaviour
 {
@@ -8,10 +9,10 @@ public class GameSwitch : MonoBehaviour
     public GameObject Spawn;
     public GameObject Airhockey;
     public GameObject Pinball;
-    public GameObject Game3;
-    public GameObject Game4;
-    public GameObject Game5;
-    public GameObject Game6;
+    public GameObject ArcadeCabinet;
+    public GameObject CraneGame;
+    public GameObject Snake;
+    public GameObject ShootingGallery;
 
     private int gameindex;
 
@@ -27,20 +28,10 @@ public class GameSwitch : MonoBehaviour
         gamelist.Add(0, Spawn);
         gamelist.Add(1, Airhockey);
         gamelist.Add(2, Pinball);
-        gamelist.Add(3, Game3);
-        gamelist.Add(4, Game4);
-        gamelist.Add(5, Game5);
-        gamelist.Add(6, Game6);
-
-        Player.transform.position = gamelist[gameindex].transform.position;
-        Vector3 newPos = gamelist[gameindex].transform.eulerAngles;
-
-        // set rotation and flip 180
-        Player.transform.eulerAngles = new Vector3(
-            newPos.x,
-            newPos.y + 180,
-            newPos.z
-            );
+        gamelist.Add(3, ArcadeCabinet);
+        gamelist.Add(4, CraneGame);
+        gamelist.Add(5, Snake);
+        gamelist.Add(6, ShootingGallery);
     }
 
     // Update is called once per frame
@@ -49,7 +40,7 @@ public class GameSwitch : MonoBehaviour
         // Sip simulation
         if (TranslationLayer.instance.GetButtonDown(ButtonCode.KeyLeft))
         {
-            gameindex = mod(gameindex - 1, 6);
+            gameindex = mod(gameindex - 1, 7);
             Player.transform.position = gamelist[gameindex].transform.position;
             Vector3 newPos = gamelist[gameindex].transform.eulerAngles;
 
@@ -65,7 +56,7 @@ public class GameSwitch : MonoBehaviour
         // Puff
         if (TranslationLayer.instance.GetButtonDown(ButtonCode.KeyRight))
         {
-            gameindex = mod(gameindex + 1, 6);
+            gameindex = mod(gameindex + 1, 7);
             Player.transform.position = gamelist[gameindex].transform.position;
             Vector3 newPos = gamelist[gameindex].transform.eulerAngles;
 
@@ -78,6 +69,48 @@ public class GameSwitch : MonoBehaviour
 
         }
 
+        // Enter Game
+        if (TranslationLayer.instance.GetButtonDown(ButtonCode.KeyFoward))
+        {
+            switch(gameindex)
+            {
+                case 0:
+                    break;
+
+                case 1:
+                    // load airhockey
+                    SceneManager.LoadScene("Breakout_AirHockey", LoadSceneMode.Single);
+                    break;
+
+                case 2:
+                    // load pinball
+                    SceneManager.LoadScene("Breakout_Pinball", LoadSceneMode.Single);
+                    break;
+
+                case 3:
+                    // load Arcade Cabinent
+                    SceneManager.LoadScene("Breakout_ArcadeCabinet", LoadSceneMode.Single);
+                    break;
+
+                case 4:
+                    // load cranegame
+                    SceneManager.LoadScene("Breakout_CraneGame", LoadSceneMode.Single);
+                    break;
+
+                case 5:
+                    // load snake
+                    SceneManager.LoadScene("Breakout_Snake", LoadSceneMode.Single);
+                    break;
+
+                case 6:
+                    // load shooting gallery
+                    SceneManager.LoadScene("Breakout_ShootingGallery", LoadSceneMode.Single);
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 
     // An always positive mod
