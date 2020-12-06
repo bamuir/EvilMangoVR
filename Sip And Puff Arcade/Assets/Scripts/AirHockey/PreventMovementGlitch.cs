798965003c6ef5a72a23ed7f2883ddd0baa862a6
dylanjwendt <dylanjwendt@aol.com>
2020-12-05 22:55:24 -0600

    Fixed Wandering Bumpers
    fixes glitch where airhockey bumpers would move even though they were locked in place through frozen rigidbody.

---
+++ Sip And Puff Arcade/Assets/Scripts/AirHockey/PreventMovementGlitch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreventMovementGlitch : MonoBehaviour
+{
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = startPos;
+    }
+}
